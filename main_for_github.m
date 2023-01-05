312931e506fbfc4ba35a07b84c85701b0caf780f
agchesebro <76024790+agchesebro@users.noreply.github.com>
2023-01-05 11:12:27 -0500

    Updated main to include new figures
    Updated to include Figures 2c, 3c, and 4c (added in response to reviewers)

--- main_for_github.m
+++ main_for_github.m
@@ -34,7 +34,7 @@ C = 0; %coupling constant (for single region, so no self-coupling)
 u = 0; %coupling "matrix" (again, only single region)
 
 %% Figure 2b: Na gradient effects
-sim_len = 2e3; %ms
+sim_len = 9e5; %ms
 step_size = 0.1; %ms
 fs = 1e3/step_size;
 
@@ -79,12 +79,30 @@ xlabel('Time (ms)', 'FontSize', 20)
 ylabel('V_i')
 ax = gca;
 ax.FontSize = 16;
-legend({'Normal', 'Depolarized', 'Hyperpolarized'})
-title('Figure 2a')
+legend({'Between NS and PD', 'Below NS', 'Above PD'})
+title('Figure 2b')
 
-%% Figure 3: Ca
+%% Figure 2c: Na gradient attractor changes
 
-sim_len = 2e3; %ms
+f = figure;
+hold on
+f.Position = [100, 100, 700, 500];
+lh = plot(y1(1e3:end, 3), y1(1e3:end, 2));
+lh.Color = [0.8500, 0.3250, 0.0980, 0.05];
+lh = plot(y2(1e3:end, 3), y2(1e3:end, 2));
+lh.Color = [0, 0.4470, 0.7410, 0.05];
+lh = plot(y3(1e3:end, 3), y3(1e3:end, 2));
+lh.Color = [0.9290, 0.6940, 0.1250, 0.2];
+xlabel('W', 'FontSize', 20)
+xlim([-0.05, 0.9])
+ylabel('Z')
+ax = gca;
+ax.FontSize = 16;
+title('Figure 2c')
+
+%% Figure 3b: Ca gradient effects
+
+sim_len = 9e5; %ms
 step_size = 0.1; %ms
 fs = 1e3/step_size;
 
@@ -127,11 +145,28 @@ xlabel('Time (ms)', 'FontSize', 20)
 ylabel('V_i')
 ax = gca;
 ax.FontSize = 16;
-legend({'Normal', 'Depolarized', 'Hyperpolarized'})
-title('Figure 3a')
+legend({'Between NS and PD', 'Below NS', 'Above PD'})
+title('Figure 3b')
 
-%% Figure 4: K
-sim_len = 2e3; %ms
+% Figure 3c: Ca gradient attractor changes
+
+f = figure;
+hold on
+f.Position = [100, 100, 700, 500];
+lh = plot(y1(1e3:end, 3), y1(1e3:end, 2));
+lh.Color = [0.8500, 0.3250, 0.0980, 0.05];
+lh = plot(y2(1e3:end, 3), y2(1e3:end, 2));
+lh.Color = [0, 0.4470, 0.7410, 0.05];
+lh = plot(y3(1e3:end, 3), y3(1e3:end, 2));
+lh.Color = [0.9290, 0.6940, 0.1250, 0.05];
+xlabel('W', 'FontSize', 20)
+ylabel('Z')
+ax = gca;
+ax.FontSize = 16;
+title('Figure 3c')
+
+%% Figure 4: K gradient effects
+sim_len = 9e5; %ms
 step_size = 0.1; %ms
 fs = 1e3/step_size;
 delta_V = 0.66;
@@ -139,11 +174,11 @@ C = 0;
 u = 0;
 
 % Random initial conditions
-y_0 = [0.1*rand(size(u, 1)*2, 1)-0.05; rand(size(u, 1), 1)];
+%y_0 = [0.1*rand(size(u, 1)*2, 1)-0.05; rand(size(u, 1), 1)];
 
 % Any initial parameters will work to generate a *similar* figure. If you
 % want the exact figure from the paper, use these initial conditions.
-%y_0 = [-0.0108434788242700;0.0169980902375737;0.381357224224074];
+y_0 = [-0.0108434788242700;0.0169980902375737;0.381357224224074];
 
 V_Na = 0.53;
 V_Ca = 1.0;
@@ -158,7 +193,7 @@ V_K = -0.59;
 V_K = -0.7;
 [t, y2] = ode45(@(t,y) lb_for_ode45(t, y, delta_V, C, u, V_Na, V_K, V_Ca), [0:step_size:sim_len], y_0);
 
-V_K = -1.0;
+V_K = -0.9;
 [t, y3] = ode45(@(t,y) lb_for_ode45(t, y, delta_V, C, u, V_Na, V_K, V_Ca), [0:step_size:sim_len], y_0);
 
 % Select range of usable figure (100ms once it's past the initial
@@ -174,15 +209,33 @@ x = x/fs*1e3;
 f = figure;
 hold on
 f.Position = [100, 100, 900, 500];
-plot(x, y2(range+7.8e2, 1), 'LineWidth', 2)
-plot(x, y1(range-3.5e2, 1), 'LineWidth', 2)
-plot(x, y3(range, 1), 'LineWidth', 2)
+plot(x, y2(range+8.1e2, 1), 'LineWidth', 2)
+plot(x, y3(range-8e2, 1), 'LineWidth', 2)
+plot(x, y1(range-2.2e2, 1), 'LineWidth', 2)
 xlabel('Time (ms)', 'FontSize', 20)
 ylabel('V_i', 'FontSize', 20)
 ax = gca;
 ax.FontSize = 16;
-legend({'Normal', 'Depolarized', 'Hyperpolarized'})
-title('Figure 4a')
+legend({'Below PD (physiological)', 'Far below PD (non-physiological)', 'Above PD'})
+%title('Figure 4b')
+
+% Figure 4c: K gradient attractor changes
+
+f = figure;
+f.Position = [100, 100, 700, 500];
+hold on
+lh = plot(y3(1e3:end, 3), y3(1e3:end, 2));
+lh.Color = [0.8500, 0.3250, 0.0980, 0.008];
+grid
+lh = plot(y2(1e3:end, 3), y2(1e3:end, 2));
+lh.Color = [0, 0.4470, 0.7410, 0.05];
+lh = plot(y1(1e3:end, 3), y1(1e3:end, 2));
+lh.Color = [0.9290, 0.6940, 0.1250, 0.05];
+xlabel('W', 'FontSize', 20)
+ylabel('Z')
+ax = gca;
+ax.FontSize = 16;
+%title('Figure 4c')
 
 %% Figure 5 Coherence plots
 % As with Figures 2-4, this is split across three sections: one for each
@@ -263,7 +316,7 @@ ax = gca;
 ax.FontSize = 16;
 colorbar
 hold on
-plot3(0.1, 0.53, 0.8, 'k+', 'MarkerSize', 20, 'LineWidth', 5) %add cross to physiological value
+%plot3(0.1, 0.53, 0.8, 'k+', 'MarkerSize', 20, 'LineWidth', 5) %add cross to physiological value
 
 %% Computing Ca replicates
 
@@ -370,5 +423,168 @@ colorbar
 ylim([-0.8 -0.62])
 hold on
 plot3(0.1, -0.7,0.8, 'k+', 'MarkerSize', 20, 'LineWidth', 5) %add cross to physiological value
+
+
+%% Supplemental Figure Plots
+% This is the code to generate the supplemental attractors that support the
+% main results. In each case, the first 1e3 timesteps are omitted to remove
+% abberantly large trajectories caused by random initial conditions.
+clear all; clc;
+
+delta_V = 0.66; %variability of excitatory/inhibitory threshold
+C = 0; %coupling constant (for single region, so no self-coupling)
+u = 0; %coupling "matrix" (again, only single region)
+sim_len = 9e5; %ms
+step_size = 0.1; %ms
+
+
+%% Figure ??? Na gradient attractors
+% Any initial parameters will work to generate a *similar* figure. If you
+% want the exact figure from the paper, use these initial conditions.
+y_0 = [-0.00827329309156305;-0.0450345569674258;0.902716109915281];
+% Random initial conditions (comment line above and uncomment this to have
+% your own initial conditions)
+%y_0 = [0.1*rand(size(u, 1)*2, 1)-0.05; rand(size(u, 1), 1)];
+V_K = -0.7; %default K reversal potential
+V_Ca = 1.0; %default Ca reversal potential
+
+V_Na = 0.38;
+[t, y1] = ode45(@(t,y) lb_for_ode45(t, y, delta_V, C, u, V_Na, V_K, V_Ca), [0:step_size:sim_len], y_0);
+V_Na = 0.53;
+[t, y2] = ode45(@(t,y) lb_for_ode45(t, y, delta_V, C, u, V_Na, V_K, V_Ca), [0:step_size:sim_len], y_0);
+V_Na = 0.68;
+[t, y3] = ode45(@(t,y) lb_for_ode45(t, y, delta_V, C, u, V_Na, V_K, V_Ca), [0:step_size:sim_len], y_0);
+V_Na = 0.46;
+[t, y4] = ode45(@(t,y) lb_for_ode45(t, y, delta_V, C, u, V_Na, V_K, V_Ca), [0:step_size:sim_len], y_0);
+V_Na = 0.58;
+[t, y5] = ode45(@(t,y) lb_for_ode45(t, y, delta_V, C, u, V_Na, V_K, V_Ca), [0:step_size:sim_len], y_0);
+
+figure
+lh = plot3(y1(3e5:end, 1), y1(3e5:end, 3), y1(3e5:end, 2));
+lh.Color = [0, 0, 0, 0.05];
+ax = gca;
+ax.CameraPosition = [10.0116   -3.6245    1.9525];
+ax.FontSize = 16;
+fig = gcf;
+fig.Position = [100, 100, 900, 700];
+grid
+xlim([-1 0.5])
+ylim([0 0.9])
+zlim([0.06 0.4])
+title('Depolarized Na Attractor')
+
+figure
+lh = plot3(y2(3e5:end, 1), y2(3e5:end, 3), y2(3e5:end, 2));
+lh.Color = [0, 0, 0, 0.05];
+ax = gca;
+ax.CameraPosition = [10.0116   -3.6245    1.9525];
+ax.FontSize = 16;
+fig = gcf;
+fig.Position = [100, 100, 900, 700];
+grid
+xlim([-1 0.5])
+ylim([0 0.9])
+zlim([0.06 0.4])
+title('Normal Na Attractor')
+
+figure
+lh = plot3(y3(3e5:end, 1), y3(3e5:end, 3), y3(3e5:end, 2));
+lh.Color = [0, 0, 0, 0.05];
+ax = gca;
+ax.CameraPosition = [10.0116   -3.6245    1.9525];
+ax.FontSize = 16;
+fig = gcf;
+fig.Position = [100, 100, 900, 700];
+grid
+xlim([-1 0.5])
+ylim([0 0.9])
+zlim([0.06 0.4])
+title('Hyperpolarized Na Attractor')
+
+%% Figure ??? Ca gradient attractors
+
+% Any initial parameters will work to generate a *similar* figure. If you
+% want the exact figure from the paper, use these initial conditions.
+y_0 = [0.0422331997796276;0.0270954220673924;0.0426598559350487];
+
+% Random initial conditions (comment line above and uncomment this to have
+% your own initial conditions)
+%y_0 = [0.1*rand(size(u, 1)*2, 1)-0.05; rand(size(u, 1), 1)];
+
+V_Na = 0.53; %default sodium reversal potential
+V_K = -0.7; %default potassium reversal potential
+
+V_Ca = 0.949;
+[t, y1] = ode45(@(t,y) lb_for_ode45(t, y, delta_V, C, u, V_Na, V_K, V_Ca), [0:step_size:sim_len], y_0);
+
+V_Ca = 1.0;
+[t, y2] = ode45(@(t,y) lb_for_ode45(t, y, delta_V, C, u, V_Na, V_K, V_Ca), [0:step_size:sim_len], y_0);
+
+V_Ca = 1.134;
+[t, y3] = ode45(@(t,y) lb_for_ode45(t, y, delta_V, C, u, V_Na, V_K, V_Ca), [0:step_size:sim_len], y_0);
+
+figure
+lh = plot3(y1(1e3:end, 1), y1(1e3:end, 3), y1(1e3:end, 2));
+lh.Color = [0, 0, 0, 0.05];
+ax = gca;
+ax.CameraPosition = [10.0116   -3.6245    1.9525];
+ax.FontSize = 16;
+fig = gcf;
+fig.Position = [100, 100, 900, 700];
+grid
+xlim([-1 0.5])
+ylim([0 0.9])
+zlim([0.06 0.25])
+title('Depolarized Ca Attractor')
+
+figure
+lh = plot3(y2(1e3:end, 1), y2(1e3:end, 3), y2(1e3:end, 2));
+lh.Color = [0, 0, 0, 0.05];
+ax = gca;
+ax.CameraPosition = [10.0116   -3.6245    1.9525];
+ax.FontSize = 16;
+fig = gcf;
+fig.Position = [100, 100, 900, 700];
+grid
+xlim([-1 0.5])
+ylim([0 0.9])
+zlim([0.06 0.25]) 
+title('Normal Ca Attractor')
+
+figure
+lh = plot3(y3(1e3:end, 1), y3(1e3:end, 3), y3(1e3:end, 2));
+lh.Color = [0, 0, 0, 0.05];
+ax = gca;
+ax.CameraPosition = [10.0116   -3.6245    1.9525];
+ax.FontSize = 16;
+fig = gcf;
+fig.Position = [100, 100, 900, 700];
+grid
+xlim([-1 0.5])
+ylim([0 0.9])
+zlim([0.06 0.25])
+title('Hyperpolarized Ca Attractor')
+
+%% Figure ??? K gradient attractors
+% Random initial conditions
+y_0 = [0.1*rand(size(u, 1)*2, 1)-0.05; rand(size(u, 1), 1)];
+
+% Any initial parameters will work to generate a *similar* figure. If you
+% want the exact figure from the paper, use these initial conditions.
+%y_0 = [-0.0108434788242700;0.0169980902375737;0.381357224224074];
+
+V_Na = 0.53;
+V_Ca = 1.0;
+
+V_K = -0.59;
+[t, y1] = ode45(@(t,y) lb_for_ode45(t, y, delta_V, C, u, V_Na, V_K, V_Ca), [0:step_size:sim_len], y_0);
+
+V_K = -0.7;
+[t, y2] = ode45(@(t,y) lb_for_ode45(t, y, delta_V, C, u, V_Na, V_K, V_Ca), [0:step_size:sim_len], y_0);
+
+V_K = -0.9;
+[t, y3] = ode45(@(t,y) lb_for_ode45(t, y, delta_V, C, u, V_Na, V_K, V_Ca), [0:step_size:sim_len], y_0);
+
+
 
 
